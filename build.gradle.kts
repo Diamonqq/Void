214d853b8dc1612719c38cfe38a3bf0233d37354
Conutik <khawaj.zaid@gmail.com>
2023-04-13 00:36:05 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,16 +23,16 @@ loom {
             property("mixin.debug", "true")
             property("asmhelper.verbose", "true")
             arg("--tweakClass", "org.spongepowered.asm.launch.MixinTweaker")
-            arg("--mixin", "mixins.void.json")
+            arg("--mixin", "mixins.examplemod.json")
         }
     }
     forge {
         pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
-        mixinConfig("mixins.void.json")
+        mixinConfig("mixins.examplemod.json")
     }
 
     mixin {
-        defaultRefmapName.set("mixins.void.refmap.json")
+        defaultRefmapName.set("mixins.examplemod.refmap.json")
     }
 }
 
@@ -83,7 +83,7 @@ tasks.withType(Jar::class) {
         this["FMLCorePluginContainsFMLMod"] = "true"
         this["ForceLoadAsMod"] = "true"
         this["TweakClass"] = "org.spongepowered.asm.launch.MixinTweaker"
-        this["MixinConfigs"] = "mixins.void.json"
+        this["MixinConfigs"] = "mixins.examplemod.json"
     }
 }
 
