da7b7024e4e87f62299d4b57e234d818b3934c31
Bear <132583423+BearOfSkill@users.noreply.github.com>
2023-07-04 01:39:21 -0500

    Adds toggleable features

--- src/main/java/com/conutik/features/AutoBuy.java
+++ src/main/java/com/conutik/features/AutoBuy.java
@@ -5,13 +5,18 @@ import com.conutik.classes.QueueItem;
 import com.conutik.config.Settings;
 import com.conutik.helpers.*;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.inventory.GuiChest;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerChest;
+import net.minecraft.inventory.Slot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -20,8 +25,13 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.conutik.helpers.Utils.ScoreboardData;
 import static com.conutik.helpers.Utils.setTimeout;
@@ -31,6 +41,7 @@ public class AutoBuy {
     private static boolean bedStarted = false;
 
     public static Thread bedThread = null;
+    public static Thread nuggetThread = null;
     public final ExecutorService tickThreadPool = Executors.newFixedThreadPool(2);
 
     private final FlipperMessage flipper = new FlipperMessage();
@@ -52,7 +63,7 @@ public class AutoBuy {
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onInventoryRendering(GuiScreenEvent.DrawScreenEvent.Post post) {
-        if(Settings.autoBuy && Utils.isWorking && (post.gui instanceof GuiChest)) {
+        if (Settings.autoBuy && Utils.isWorking && (post.gui instanceof GuiChest)) {
             ContainerChest chest = (ContainerChest) ((GuiChest) post.gui).inventorySlots;
             if (chest != null) {
                 String name = chest.getLowerChestInventory().getName();
@@ -61,31 +72,57 @@ public class AutoBuy {
                     if (stack != null) {
                         if (Items.feather != stack.getItem()) {
                             if (Items.potato == stack.getItem()) {
-                                Helpers.sendDebugMessage("Someone bought the auction already, skipping...");
-                                handleBuyFinished();
-                                Minecraft.getMinecraft().thePlayer.closeScreen();
+                                new Thread(() -> {
+                                    Helpers.sendDebugMessage("Someone bought the auction already, skipping...");
+                                    if (Settings.sendFlipBuyer) {
+                                        NBTTagCompound tagCompound = stack.getTagCompound();
+                                        NBTTagList tagList = tagCompound.getCompoundTag("display").getTagList("Lore", 8);
+                                        java.util.List<String> loreList = new ArrayList<>();
+                                        for (int i = 0; i < tagList.tagCount(); i++) {
+                                            loreList.add(tagList.getStringTagAt(i));
+                                        }
+                                        String buyer = null;
+                                        for (String loreLine : loreList) {
+                                            if (!loreLine.startsWith("Buyer: ")) continue;
+                                            buyer = loreLine.split("Buyer: ")[1];
+                                        }
+                                        String itemName = loreList.get(1);
+                                        if (Minecraft.getMinecraft().gameSettings.advancedItemTooltips)
+                                            itemName.replace("[()#\\/0-9]+$", "");
+                                        Helpers.sendChatMessage(itemName + " was bought by " + buyer + ".");
+                                    }
+                                    handleBuyFinished();
+                                    try {
+                                        Thread.sleep(50);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                    Minecraft.getMinecraft().thePlayer.closeScreen();
+                                }).start();
                             } else if (Items.bed == stack.getItem() && !bedStarted) {
-//                                Helpers.sendDebugMessage("Old bed :D");
                                 bedStarted = true;
                                 bedThread = new Thread(() -> {
                                     int loopInt = 0;
                                     try {
                                         while (loopInt < 100) {
-                                            loopInt = loopInt + 1;
+                                            loopInt++;
                                             if (chest.getLowerChestInventory().getName().contains("BIN Auction View")) {
                                                 clickNugget(chest.windowId);
-                                                clickConfirm(chest.windowId+1);
+                                                if (Settings.fullSkip)
+                                                    clickConfirm(chest.windowId+1);
                                                 Thread.sleep(Settings.bedDelay);
-                                            }}
+                                            }
+                                        }
                                     } catch (InterruptedException e) {
                                         e.printStackTrace();
                                     }
                                 });
                                 bedThread.start();
-                                setTimeout(AutoBuy::handleBuyFinished, 5000);
+                                setTimeout(AutoBuy::handleBuyFinished, Settings.bedThreadKillDelay);
                             } else if (Items.gold_nugget == stack.getItem() || Item.getItemFromBlock(Blocks.gold_block) == stack.getItem()) {
                                 clickNugget(chest.windowId);
-                                clickConfirm(chest.windowId+1);
+                                if (Settings.fullSkip)
+                                    clickConfirm(chest.windowId+1);
                             }
                         }
                     }
@@ -101,7 +138,7 @@ public class AutoBuy {
 
     @SubscribeEvent
     public void onClientChatMessage(ClientChatReceivedEvent event) {
-        if(event.type == 2) return;
+        if (event.type == 2) return;
         String str = event.message.getUnformattedText();
         if (str.contains("This auction wasn't found") || str.contains("There was an error with the auction")) {
             Helpers.sendDebugMessage("Error or not found");
@@ -117,7 +154,7 @@ public class AutoBuy {
             Macro.getInstance().getQueue().setRunning(false);
             Minecraft.getMinecraft().thePlayer.closeScreen();
         }
-        if (str.contains("You purchased ") && !Settings.flipWebhookURL.isEmpty()) {
+        if (str.contains("You purchased ") && !Settings.buyWebhookURL.isEmpty()) {
             Helpers.sendDebugMessage("Purchased an item");
             try {
                 flipper.itemBought(str);
@@ -125,16 +162,16 @@ public class AutoBuy {
                 Helpers.sendDebugMessage("An error occured while trying to send webhook.");
             }
         }
-        if(str.equals("You were spawned in Limbo.") && Settings.antiLimbo && Utils.isWorking) {
+        if (str.equals("You were spawned in Limbo.") && Settings.antiLimbo && Utils.isWorking) {
             Helpers.sendDebugMessage("Found you in, taking you back to lobby");
             Utils.setTimeout(() -> {
                 lastCommandRan = "/lobby";
                 (Minecraft.getMinecraft()).thePlayer.sendChatMessage("/lobby");
-                if(Settings.autoLobby) Utils.setTimeout(() -> {
+                if (Settings.antiLobby) Utils.setTimeout(() -> {
                     lastCommandRan = "/play sb";
                     Helpers.sendDebugMessage("You in the lobby not skyblock, let me change that rq");
                     (Minecraft.getMinecraft()).thePlayer.sendChatMessage("/play sb");
-                    if(Settings.autoIsland) Utils.setTimeout(() -> {
+                    if (Settings.autoIsland) Utils.setTimeout(() -> {
                         lastCommandRan = "/is";
                         Helpers.sendDebugMessage("Kicking you back to your minions");
                         (Minecraft.getMinecraft()).thePlayer.sendChatMessage("/is");
@@ -143,58 +180,92 @@ public class AutoBuy {
             }, 2500);
         }
 
-        if(str.contains("Cannot join SkyBlock for a moment!") && Settings.autoLobby && Utils.isWorking) {
+        if (str.contains("Cannot join SkyBlock for a moment!") && Settings.antiLobby && Utils.isWorking) {
             Helpers.sendDebugMessage("Trying to push you into skyblock again");
             Utils.setTimeout(() -> {
                 lastCommandRan = "/play sb";
                 Minecraft.getMinecraft().thePlayer.sendChatMessage("/play sb");
             }, 2500);
         }
-        if(str.contains("You are sending commands too fast!") && Settings.autoLobby && Utils.isWorking) {
+        if (str.contains("You are sending commands too fast!") && Settings.antiLobby && Utils.isWorking) {
             Helpers.sendDebugMessage("Running commands quickly? slow down mcqueen");
             Utils.setTimeout(() -> {
-                if(lastCommandRan != null) Minecraft.getMinecraft().thePlayer.sendChatMessage(lastCommandRan);
+                if (lastCommandRan != null) Minecraft.getMinecraft().thePlayer.sendChatMessage(lastCommandRan);
             }, 2500);
         }
-        if(str.contains("[Auction]") && str.contains("bought") && !event.message.getChatStyle().isEmpty()) {
-            String action = event.message.getChatStyle().getChatClickEvent().getValue();
-            if(Settings.autoClaim && Utils.isWorking) {
-                Minecraft.getMinecraft().thePlayer.sendChatMessage(action);
-                Utils.setTimeout(() -> {
-                    Minecraft.getMinecraft().playerController.windowClick(Minecraft.getMinecraft().thePlayer.openContainer.windowId, 31, 0, 3, Minecraft.getMinecraft().thePlayer);
-                    Helpers.sendDebugMessage("Auto claimed auction :D");
-                }, 1000);
-            }
-            if(!Settings.purchaseWebhooks) return;
-            str = str.replaceAll("§.", "");
-            String purchaser = str.split("\\[Auction] ")[1].split("bought")[0];
-            String price = str.split("for ")[1].split("coins")[0];
-            String item = str.split("bought ")[1].split("for")[0];
+
+        final String captchaString = "^Hello there, you acted suspiciously.*|^\\[Coflnet\\]:.*likely being afk.*";
+        final Pattern captchaPattern = Pattern.compile(captchaString);
+        final Matcher captchaMatcher = captchaPattern.matcher(str);
+        if (captchaMatcher.find()) {
+        //if (str.contains("Hello there, you acted suspiciously like a macro bot")) {
             String content = "<@" + Settings.discordID + ">";
-            if(Settings.discordID.isEmpty()) content = "Flip sold";
-            if(Settings.purchaseWebhookURL.isEmpty()) return;
+            if (!Settings.captchaWebhooks) return;
+            if (Settings.discordID.isEmpty()) content = "A Captcha appeared";
+            if (Settings.captchaWebhookURL.isEmpty()) return;
             String description = null;
-            if(!Settings.purchaseWebhookDescription.isEmpty()) description = Settings.purchaseWebhookDescription;
-            Webhook purchaseWebhook = new Webhook(Settings.purchaseWebhookURL);
-            purchaseWebhook.setContent(content);
-            purchaseWebhook.setAvatarUrl("https://cdn.discordapp.com/icons/1074429357642227853/f2f3c75446e8774afe5c448d83401153.png");
-            purchaseWebhook.setUsername("Void Flipper");
-            purchaseWebhook.addEmbed(
-                    new Webhook.EmbedObject()
-                            .setTitle("Item Sold")
-                            .setColor(Settings.purchaseWebhookColor)
-                            .addField("Item:", item, false)
-                            .addField("Purchaser:", purchaser, false)
-                            .addField("Price:", price, false)
-                            .setDescription(description)
-                            .setThumbnail("https://minotar.net/helm/" + Minecraft.getMinecraft().thePlayer.getDisplayNameString() + "/600.png")
+            if (!Settings.captchaWebhookDescription.isEmpty()) description = Settings.captchaWebhookDescription;
+            Webhook captchaWebhook = new Webhook(Settings.captchaWebhookURL);
+            captchaWebhook.setContent(content);
+            captchaWebhook.setAvatarUrl("https://cdn.discordapp.com/icons/1074429357642227853/f2f3c75446e8774afe5c448d83401153.png");
+            captchaWebhook.setUsername("Void Webhook");
+            captchaWebhook.addEmbed(
+                new Webhook.EmbedObject()
+                    .setTitle("Captcha has appeared")
+                    .setColor(Settings.captchaColor)
+                    .setDescription("```A WILD CAPTCHA HAS APPEARED. SOLVE IT TO CONTINUE YOUR FLIPPING JOURNEY !!!```\n" + description)
+                    .setThumbnail("https://minotar.net/helm/" + Minecraft.getMinecraft().thePlayer.getDisplayNameString() + "/600.png")
             );
+            Helpers.sendDebugMessage("Captcha Webhook executed.");
             try{
-                purchaseWebhook.execute();
+                captchaWebhook.execute();
             } catch (IOException e) {
-                e.printStackTrace();
-                Helpers.sendDebugMessage(e.toString());
+                Helpers.sendDebugMessage(e.getMessage());
             }
+        }
+
+        if (str.contains("[Auction]") && str.contains("bought") && !event.message.getChatStyle().isEmpty()) {
+            new Thread(() -> {
+                String action = event.message.getChatStyle().getChatClickEvent().getValue();
+                if (Settings.autoClaim && Utils.isWorking) {
+                    //if (BIN view/AH open)
+                    Minecraft.getMinecraft().thePlayer.sendChatMessage(action);
+                    Utils.setTimeout(() -> {
+                        Minecraft.getMinecraft().playerController.windowClick(Minecraft.getMinecraft().thePlayer.openContainer.windowId, 31, 0, 3, Minecraft.getMinecraft().thePlayer);
+                        Helpers.sendDebugMessage("Auto-Claimed sold auction.");
+                    }, 1000);
+                }
+                if (!Settings.soldWebhooks) return;
+                String string = str.replaceAll("§.", "");
+                String buyer = string.split("\\[Auction] ")[1].split("bought")[0];
+                String price = string.split("for ")[1].split("coins")[0];
+                String item = string.split("bought ")[1].split("for")[0];
+                String content = "<@" + Settings.discordID + ">";
+                if (Settings.discordID.isEmpty()) content = "Flip sold";
+                if (Settings.soldWebhookURL.isEmpty()) return;
+                String description = null;
+                if (!Settings.soldWebhookDescription.isEmpty()) description = Settings.soldWebhookDescription;
+                Webhook soldWebhook = new Webhook(Settings.soldWebhookURL);
+                soldWebhook.setContent(content);
+                soldWebhook.setAvatarUrl("https://cdn.discordapp.com/icons/1074429357642227853/f2f3c75446e8774afe5c448d83401153.png");
+                soldWebhook.setUsername("Void Webhook");
+                soldWebhook.addEmbed(
+                    new Webhook.EmbedObject()
+                        .setTitle("Item Sold")
+                        .setColor(Settings.soldWebhookColor)
+                        .addField("Item:", item, false)
+                        .addField("Buyer:", buyer, false)
+                        .addField("Price:", price, false)
+                        .setDescription(description)
+                        .setThumbnail("https://minotar.net/helm/" + Minecraft.getMinecraft().thePlayer.getDisplayNameString() + "/600.png")
+                );
+                try {
+                    soldWebhook.execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Helpers.sendDebugMessage(e.toString());
+                }
+            }).start();
         }
     }
 
@@ -232,4 +303,3 @@ public class AutoBuy {
         (Minecraft.getMinecraft()).playerController.windowClick(id, index, 0, 3, Minecraft.getMinecraft().thePlayer);
     }
 }
-
